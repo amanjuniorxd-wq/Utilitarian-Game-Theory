aacce374357b3c342a0f8d47fb627be0da05d729
amanjuniorxd-wq <amanjuniorxd@gmail.com>
2026-09-03 13:57:40 +0530

    Make Android app load Wealth Grid

--- wealth-distribution/android/app/src/main/java/in/mishrin/wealthgrid/MainActivity.java
+++ wealth-distribution/android/app/src/main/java/in/mishrin/wealthgrid/MainActivity.java
@@ -1,3 +1,3 @@
 package in.mishrin.wealthgrid;
 import android.app.Activity;import android.os.Bundle;import android.webkit.WebView;import android.webkit.WebSettings;
-public class MainActivity extends Activity{public void onCreate(Bundle b){super.onCreate(b);WebView w=new WebView(this);w.setBackgroundColor(0xfff5f7f2);WebSettings s=w.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);w.loadUrl("file:///android_asset/index.html");setContentView(w);}}
+public class MainActivity extends Activity{public void onCreate(Bundle b){super.onCreate(b);WebView w=new WebView(this);w.setBackgroundColor(0xfff5f7f2);WebSettings s=w.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);w.loadUrl("https://raw.githubusercontent.com/amanjuniorxd-wq/Utilitarian-Game-Theory/main/wealth-distribution/index.html");setContentView(w);}}
